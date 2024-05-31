737a00110e64f21a49ff3790004579f3d7243c72
codenexusplus <misscode110@gmail.com>
2024-05-31 10:21:13 -0600

    Adding a simple calculator

---
+++ index.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//printing a wellcoming message
+console.log("\n\tWellcome To \'CodeWithMaha\' -CLI Simple Calculator\n");
+//asking questions from users through inqurier
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Multiplication", "Division", "Subtraction"],
+    },
+]);
+console.log(answers);
+//conditional statements If-Else
+//if our user choose addition then
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+//if our user choose subtraction then
+else if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+//if our user choose division then
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+//if our user choose multiplication then
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+//if non of these options
+else {
+    console.log("Invalid Input");
+}
